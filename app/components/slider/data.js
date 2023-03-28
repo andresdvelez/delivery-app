a24bd4ee7f8984e1015a354022632102ce7a06cc
Andrés Vélez <87771860+andresdvelez@users.noreply.github.com>
2023-03-28 10:06:15 -0500

    create sign-up slider

---
+++ app/components/slider/data.js
@@ -0,0 +1,17 @@
+export const sliderData = [
+  {
+    id: 1,
+    text: "Choose what to eat choosing from  a variety of restaurants from the list",
+    img: "/slider-1.svg",
+  },
+  {
+    id: 2,
+    text: "Choose where you want to deliver by indicating on the map",
+    img: "/slider-2.svg",
+  },
+  {
+    id: 3,
+    text: "We will deliver as soon as possible",
+    img: "/slider-3.svg",
+  },
+];
