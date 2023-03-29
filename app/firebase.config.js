f728bb7e983657430b2cf862899c2d5667c482da
Andrés Vélez <87771860+andresdvelez@users.noreply.github.com>
2023-03-29 17:34:38 -0500

    create signup && signin flow

---
+++ app/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZZlqBBGCJrHAJBxPNAf_eeuQQeH7RO-E",
+  authDomain: "delivery-app-73554.firebaseapp.com",
+  projectId: "delivery-app-73554",
+  storageBucket: "delivery-app-73554.appspot.com",
+  messagingSenderId: "75606763607",
+  appId: "1:75606763607:web:8b43b95801ee63cf1adb12",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ app/signup/location/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function location() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default location
