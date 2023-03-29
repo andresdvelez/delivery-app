e53804b5a736e19d69a617363532317c724e99c6
Andrés Vélez <87771860+andresdvelez@users.noreply.github.com>
2023-03-29 18:02:16 -0500

    create location picker screen

--- app/signup/location/page.jsx
+++ app/signup/location/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+
+import map from "@/public/map.png";
 
 function location() {
   return (
-    <div>page</div>
-  )
+    <section className="px-4 pt-24 flex flex-col items-center">
+      <p className="text-sm text-center w-60 mb-8">
+        Allow access to geo data on the device mark your address on the map
+      </p>
+      <div>
+        <figure className="w-full">
+          <Image src={map} width="120%" height="100%" />
+        </figure>
+        <p className="text-end w-full text-[10px] mt-3">Where am i?</p>
+      </div>
+      <button className="bg-main-color rounded-xl h-10 mt-60 w-full">Submit</button>
+    </section>
+  );
 }
 
-export default location
+export default location;
